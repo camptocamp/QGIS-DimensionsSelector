9346de5266b0958ead274d63a4607531ec5109d9
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2018-07-12 15:42:30 +0200

    Add locale fr

---
+++ i18n/fr.ts
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="test_translations.py" line="48"/>
+        <source>Good morning</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DimensionsSelectorPlugin</name>
+    <message>
+        <location filename="dimensions_selector_plugin.py" line="123"/>
+        <source>&amp;Dimensions Selector</source>
+        <translation>Sélecteur de &amp;dimensions</translation>
+    </message>
+    <message>
+        <location filename="dimensions_selector_plugin.py" line="98"/>
+        <source>Settings</source>
+        <translation>Réglages</translation>
+    </message>
+    <message>
+        <location filename="dimensions_selector_plugin.py" line="106"/>
+        <source>Toggle filtering</source>
+        <translation>Activation du filtrage</translation>
+    </message>
+</context>
+<context>
+    <name>DimensionsTableModel</name>
+    <message>
+        <location filename="settings_dialog.py" line="114"/>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <location filename="settings_dialog.py" line="118"/>
+        <source>Choices</source>
+        <translation>Valeurs</translation>
+    </message>
+    <message>
+        <location filename="settings_dialog.py" line="122"/>
+        <source>Active</source>
+        <translation>Activé</translation>
+    </message>
+</context>
+<context>
+    <name>LayerDimensionsTableModel</name>
+    <message>
+        <location filename="settings_dialog.py" line="162"/>
+        <source>Layer</source>
+        <translation>Couche</translation>
+    </message>
+    <message>
+        <location filename="settings_dialog.py" line="167"/>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <location filename="settings_dialog.py" line="171"/>
+        <source>Field</source>
+        <translation>Champ</translation>
+    </message>
+    <message>
+        <location filename="settings_dialog.py" line="175"/>
+        <source>Active</source>
+        <translation>Activé</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialogBase</name>
+    <message>
+        <location filename="settings_dialog.ui" line="14"/>
+        <source>Dimensions Selector Settings</source>
+        <translation>Réglages du sélecteur de dimensions</translation>
+    </message>
+    <message>
+        <location filename="settings_dialog.ui" line="81"/>
+        <source>Add</source>
+        <translation>Ajouter</translation>
+    </message>
+    <message>
+        <location filename="settings_dialog.ui" line="88"/>
+        <source>Remove</source>
+        <translation>Retirer</translation>
+    </message>
+    <message>
+        <location filename="settings_dialog.ui" line="54"/>
+        <source>Populate</source>
+        <translation>Peupler</translation>
+    </message>
+    <message>
+        <location filename="settings_dialog.ui" line="67"/>
+        <source>Filter with selected dimension</source>
+        <translation>Filtrer avec la dimension sélectionnée</translation>
+    </message>
+</context>
+</TS>
